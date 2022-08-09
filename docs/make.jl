f46508916dca05ede156c73c759ca2a722a31b50
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-08-09 17:13:20 +0200

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using MarkovKernels
+using Documenter
+
+DocMeta.setdocmeta!(MarkovKernels, :DocTestSetup, :(using MarkovKernels); recursive = true)
+
+makedocs(;
+    modules = [MarkovKernels],
+    authors = "Filip Tronarp <filip.tronarp@uni-tuebingen.de> and contributors",
+    repo = "https://github.com/filtron/MarkovKernels.jl/blob/{commit}{path}#{line}",
+    sitename = "MarkovKernels.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://filtron.github.io/MarkovKernels.jl",
+        edit_link = "main",
+        assets = String[],
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo = "github.com/filtron/MarkovKernels.jl",
+    devbranch = "main",
+)
