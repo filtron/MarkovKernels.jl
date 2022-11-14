f41be2cb5eb6843444e9fc255c92a571cc71bf1b
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2022-11-14 19:40:49 +0100

    normalkernel tests

--- test/kernels/normalkernel_test.jl
+++ test/kernels/normalkernel_test.jl
@@ -1,17 +1,14 @@
 function normalkernel_test(T, affine_types)
-    Σ = x -> hcat(exp.(abs.(x)))
+    A = ones(T, 1, 1)
+    Σ = x -> diagm(exp.(abs.(x)))
+    K = NormalKernel(A, Σ)
+    x = randn(T, 1)
 
-    for t in affine_types
-        slope, intercept, F = _make_affinemap(T, 1, 1, t)
-        K = NormalKernel(F, Σ)
-        x = randn(T, 1)
-
-        @testset "NormalKernel | Unary | $(T) | $(t)" begin
-            #@test_nowarn show(K)
-            @test mean(K)(x) == F(x)
-            @test cov(K)(x) == Σ(x)
-            @test condition(K, x) == Normal(F(x), Σ(x))
-        end
+    @testset "NormalKernel | Unary | $(T)" begin
+        @test_nowarn repr(K)
+        @test mean(K)(x) == A * x
+        @test cov(K)(x) == Σ(x)
+        @test condition(K, x) == Normal(A * x, Σ(x))
     end
 end
 
@@ -25,32 +22,41 @@ function affine_normalkernel_test(T, n, affine_types, cov_types)
         )
     end
 
-    kernel_type_parameters = Iterators.product(affine_types, cov_types)
+    covar_types = (:HermOrSym, :Cholesky)
+    matrix_types = (:Matrix, :SMatrix)
+    eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
 
-    for ts in kernel_type_parameters
-        atype, ctype = ts
+    Ap = randn(T, n, n)
+    Vp = randn(T, n, n)
+    Vp = Vp * Vp'
+    μp = randn(T, n)
+    xp = randn(T, n)
 
-        M, cov_mat, cov_param, K = _make_normalkernel(T, n, n, atype, ctype)
-        x = randn(T, n)
+    for cov_t in covar_types, matrix_t in matrix_types
+        A = _make_matrix(Ap, matrix_t)
+        μ = _make_vector(μp, matrix_t)
+        Σ = _make_covp(_make_matrix(Vp, matrix_t), cov_t)
+        x = _make_vector(xp, matrix_t)
+        K = NormalKernel(A, Σ)
 
-        eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
-
-        @testset "AffineNormalKernel | Unary | $(T) | $(atype) | $(ctype)" begin
+        @testset "AffineNormalKernel | Unary | $(T) | $(cov_t) | $(matrix_t)" begin
             @test_nowarn repr(K)
             @test eltype(K) == T
             @test typeof(K) <: AffineNormalKernel
-            @test K == NormalKernel(mean(K)..., cov_param)
+            @test K == NormalKernel(mean(K)..., Σ)
             for U in eltypes
                 @test AbstractMarkovKernel{U}(K) ==
                       AbstractNormalKernel{U}(K) ==
                       NormalKernel{U}(K)
                 @test eltype(AbstractNormalKernel{U}(K)) == U
             end
-            @test mean(K)(x) == M(x)
-            @test cov(K)(x) == cov_param
-            @test covp(K) == cov_param
-            @test condition(K, x) == Normal(M(x), cov_param)
+            @test mean(K)(x) == A * x
+            @test _ofsametype(x, A * x)
+            @test cov(K)(x) == Σ
+            @test covp(K) == Σ
+            @test condition(K, x) == Normal(A * x, Σ)
             @test eltype(rand(K, x)) == T
+            @test _ofsametype(μ, rand(K, x))
         end
     end
 end
