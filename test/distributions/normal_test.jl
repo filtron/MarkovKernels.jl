2360a5373013ad724e694ef911bad00fabbf3444
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2022-11-14 18:16:19 +0100

    normal tests

--- test/distributions/normal_test.jl
+++ test/distributions/normal_test.jl
@@ -1,25 +1,27 @@
 function normal_test(T, n, cov_types)
-    ncovps = length(cov_types)
-    means, ncov_mats, ncov_params, normals =
-        collect(zip(map(x -> _make_normal(T, n, x), cov_types)...))
-
-    x = randn(T, n)
-
-    eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
-
     @testset "Normal | AbstractMatrix constructor" begin
         @test_throws DomainError Normal(ones(2), tril(ones(2, 2)))
         @test_throws DomainError Normal(ones(ComplexF64, 2), tril(ones(2, 2)))
         @test_throws DomainError Normal(ones(ComplexF64, 2), Symmetric(diagm(ones(2))))
     end
 
-    @testset "Normal | Unary | $(T)" begin
-        for i in 1:ncovps
-            N = normals[i]
-            μ = means[i]
-            covmat = ncov_mats[i]
-            covpar = ncov_params[i]
+    eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
+    cov_types = (:HermOrSym, :Cholesky)
+    matrix_types = (:Matrix, :SMatrix)
 
+    xp = randn(T, n)
+    μ1p = randn(T, n)
+    V1p = tril(ones(T, n, n))
+    V1p = V1p * V1p'
+
+    for cov_t in cov_types, matrix_t in matrix_types
+        x = _make_vector(xp, matrix_t)
+        μ = _make_vector(μ1p, matrix_t)
+        Σ = _make_covp(_make_matrix(V1p, matrix_t), cov_t)
+
+        N = Normal(μ, Σ)
+
+        @testset "Normal | Unary | $(T) | " begin
             @test_nowarn repr(N)
             @test eltype(N) == T
             for U in eltypes
@@ -28,14 +30,15 @@ function normal_test(T, n, cov_types)
             end
             @test N == N
             @test mean(N) == μ
-            @test cov(N) ≈ covmat
-            @test covp(N) == covpar
-            @test var(N) ≈ real.(diag(covmat))
-            @test std(N) ≈ sqrt.(real.(diag(covmat)))
+            @test cov(N) ≈ V1p
+            @test covp(N) == Σ
+            @test var(N) ≈ real.(diag(V1p))
+            @test std(N) ≈ sqrt.(real.(diag(V1p)))
 
-            @test residual(N, x) ≈ cholesky(covmat).L \ (x - μ)
-            @test logpdf(N, x) ≈ _logpdf(T, μ, covmat, x)
-            @test entropy(N) ≈ _entropy(T, μ, covmat)
+            @test residual(N, x) ≈ cholesky(V1p).L \ (x - μ)
+            @test _ofsametype(x, residual(N, x))
+            @test logpdf(N, x) ≈ _logpdf(T, μ, V1p, x)
+            @test entropy(N) ≈ _entropy(T, μ, V1p)
 
             @test eltype(var(N)) <: Real
             @test eltype(std(N)) <: Real
@@ -44,24 +47,26 @@ function normal_test(T, n, cov_types)
 
             @test length(rand(N)) == dim(N)
             @test eltype(rand(N)) == T
+            @test _ofsametype(μ, rand(N))
         end
     end
 
-    means2, ncov_mats2, ncov_params2, normals2 =
-        collect(zip(map(x -> _make_normal(T, n, x), cov_types)...))
+    μ2p = randn(T, n)
+    V2p = triu(ones(T, n, n))
+    V2p = V2p * V2p'
 
-    @testset "Normal | Binary | $(T)" begin
-        for i in 1:ncovps, j in i:ncovps
-            N1 = normals[i]
-            μ1 = means[i]
-            covmat1 = ncov_mats[i]
+    for cov_t in cov_types, matrix_t in matrix_types
+        x = _make_vector(xp, matrix_t)
+        μ1 = _make_vector(μ1p, matrix_t)
+        Σ1 = _make_covp(_make_matrix(V1p, matrix_t), cov_t)
+        N1 = Normal(μ1, Σ1)
+        μ2 = _make_vector(μ2p, matrix_t)
+        Σ2 = _make_covp(_make_matrix(V2p, matrix_t), cov_t)
+        N2 = Normal(μ2, Σ2)
 
-            N2 = normals2[j]
-            μ2 = means2[j]
-            covmat2 = ncov_mats2[j]
-
-            @test kldivergence(N1, N2) ≈ _kld(T, μ1, covmat1, μ2, covmat2)
-            @test kldivergence(N2, N1) ≈ _kld(T, μ2, covmat2, μ1, covmat1)
+        @testset "Normal | Binary | {$(T),$(cov_t),$(matrix_t)}" begin
+            @test kldivergence(N1, N2) ≈ _kld(T, μ1p, V1p, μ2p, V2p)
+            @test kldivergence(N2, N1) ≈ _kld(T, μ2p, V2p, μ1p, V1p)
             @test eltype(kldivergence(N1, N2)) <: Real
             @test eltype(kldivergence(N2, N1)) <: Real
         end
