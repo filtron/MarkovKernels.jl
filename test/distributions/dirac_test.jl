52af2fdc0e4847ed4ef3544e71d626029bbd1b24
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2022-11-17 15:07:22 +0100

    dirac tests

--- test/distributions/dirac_test.jl
+++ test/distributions/dirac_test.jl
@@ -10,10 +10,21 @@ function dirac_test(T, n)
         @test eltype(D) == T
         @test dim(D) == n
         for U in eltypes
-            AbstractDistribution{U}(D) == AbstractDirac{U}(D) == Dirac{U}(D)
-            eltype(AbstractDirac{U}(D)) == U
+            @test AbstractDistribution{U}(D) == AbstractDirac{U}(D) == Dirac{U}(D)
+            @test eltype(AbstractDirac{U}(D)) == U
         end
         @test mean(D) == μ
         @test rand(D) == mean(D)
+    end
+
+    # Dirac over trjaectories
+    μ = [[T(k)] for k in 1:5]
+    D = Dirac(μ)
+
+    @testset "TrajectoryDirac | $(T) | eltype(D)" begin
+        for U in eltypes
+            @test AbstractDistribution{U}(D) == AbstractDirac{U}(D) == Dirac{U}(D)
+            @test eltype(AbstractDirac{U}(D)) == U
+        end
     end
 end
