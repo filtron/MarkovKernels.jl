3cd8fd620224edbdcd44d53f628fd3976c280b5c
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2022-11-17 15:57:36 +0100

    test Exception branches in driac

--- test/distributions/dirac_test.jl
+++ test/distributions/dirac_test.jl
@@ -3,15 +3,20 @@ function dirac_test(T, n)
     μ = randn(T, n)
     D = Dirac(μ)
 
-    eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
-
+    compatible_eltypes = T <: Real ? (Float32, Float64) : (ComplexF32, ComplexF64)
+    incompatible_eltypes = T <: Real ? (ComplexF32, ComplexF64) : (Float32, Float64)
     @testset "Dirac | $(T) " begin
         @test_nowarn repr(D)
         @test eltype(D) == T
         @test dim(D) == n
-        for U in eltypes
+        for U in compatible_eltypes
             @test AbstractDistribution{U}(D) == AbstractDirac{U}(D) == Dirac{U}(D)
             @test eltype(AbstractDirac{U}(D)) == U
+        end
+        for U in incompatible_eltypes
+            @test_throws ErrorException AbstractDistribution{U}(D)
+            @test_throws ErrorException AbstractDirac{U}(D)
+            @test_throws ErrorException Dirac{U}(D)
         end
         @test mean(D) == μ
         @test rand(D) == mean(D)
@@ -22,9 +27,16 @@ function dirac_test(T, n)
     D = Dirac(μ)
 
     @testset "TrajectoryDirac | $(T) | eltype(D)" begin
-        for U in eltypes
+        @test eltype(D) == T
+        @test mean(D) == μ
+        for U in compatible_eltypes
             @test AbstractDistribution{U}(D) == AbstractDirac{U}(D) == Dirac{U}(D)
             @test eltype(AbstractDirac{U}(D)) == U
+        end
+        for U in incompatible_eltypes
+            @test_throws ErrorException AbstractDistribution{U}(D)
+            @test_throws ErrorException AbstractDirac{U}(D)
+            @test_throws ErrorException Dirac{U}(D)
         end
     end
 end
