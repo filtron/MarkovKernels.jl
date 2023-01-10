d6a2add63289b20b83895bc8591f5ffa07920597
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2023-01-10 19:34:39 +0100

    file for bootrstrap smoother

---
+++ demo/bootstrap_smoother.jl
@@ -0,0 +1,17 @@
+import StatsBase
+
+function bootstrap_smoother(
+    rng::AbstractRNG,
+    ys::AbstractVecOrMat,
+    init::AbstractDistribution,
+    fw_kernel::AbstractMarkovKernel,
+    m_kernel::AbstractMarkovKernel,
+    K::Integer,
+)
+
+
+
+
+
+
+end
