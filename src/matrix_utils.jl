ff4742678e04b9a618f0731db58faa6ed2c16b53
Filip Tronarp <filip.tronarp@uni-tuebingen.de>
2024-01-18 20:31:35 +0100

    branch on version for Herm fix (#87)

--- src/matrix_utils.jl
+++ src/matrix_utils.jl
@@ -1,11 +1,13 @@
 # Fix some LinearAlgebra.jl functions 
 
-function LinearAlgebra.logdet(H::Hermitian)
-    mag, sign = logabsdet(H)
-    resign = real(sign)
-    return mag + log(resign)
+if VERSION ≤ v"1.10.0"
+    function LinearAlgebra.logdet(H::Hermitian)
+        mag, sign = logabsdet(H)
+        resign = real(sign)
+        return mag + log(resign)
+    end
+    LinearAlgebra.logdet(H::HermOrSym{T,<:Diagonal}) where {T} = logdet(real.(parent(H)))
 end
-LinearAlgebra.logdet(H::HermOrSym{T,<:Diagonal}) where {T} = logdet(real.(parent(H)))
 
 LinearAlgebra.inv(H::HermOrSym{T,<:Diagonal}) where {T} =
     T <: Complex ? Hermitian(inv(parent(H))) : Symmetric(inv(parent(H)))
