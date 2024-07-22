a35e22fe5ba4a66b79866d9f70381b1c7dac4d5e
filtron <filip.tronarp@matstat.lu.se>
2024-07-22 15:04:51 +0200

    fix typo

--- src/distributions/uvnormal.jl
+++ src/distributions/uvnormal.jl
@@ -9,6 +9,6 @@ const UvNormal{T} = Normal{T,T,T} where {T<:Number}
 cov(N::UvNormal) = N.Σ
 var(N::UvNormal) = N.Σ
 
-Normal{T}(N::UvNormal) where {T} = Normal(convert(T, mean(N)), convert(T, cov(N)))
+Normal{T}(N::UvNormal) where {T} = Normal(convert(T, mean(N)), convert(T, covp(N)))
 
 rand(rng::AbstractRNG, N::UvNormal) = mean(N) + lsqrt(covp(N)) * randn(rng, eltype(N))
