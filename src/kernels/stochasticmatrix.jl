6ef48e59db8eb94bcfc64f08e238870a5ee53ff7
filtron <filip.tronarp@matstat.lu.se>
2025-10-22 17:28:43 +0200

    invert! for discrete case

--- src/kernels/stochasticmatrix.jl
+++ src/kernels/stochasticmatrix.jl
@@ -50,6 +50,33 @@ using the random number generator rng.
 rand(rng::AbstractRNG, K::AbstractStochasticMatrix, x::Int) = rand(rng, condition(K, x))
 rand(K::AbstractStochasticMatrix, x::Int) = rand(Random.default_rng(), K, x)
 
+eltype(k::AbstractStochasticMatrix) = eltype(probability_matrix(k))
+
+size(k::AbstractStochasticMatrix) = size(probability_matrix(k))
+size(k::AbstractStochasticMatrix, i) = size(k)[i]
+
+function Base.copy!(kdst::AbstractStochasticMatrix, ksrc::AbstractStochasticMatrix)
+    copy!(probability_matrix(kdst), probability_matrix(ksrc))
+    return kdst
+end
+
+similar(k::AbstractStochasticMatrix) = similar(k, eltype(k), size(k))
+similar(k::AbstractStochasticMatrix, m, n) = similar(k, (m, n))
+similar(k::AbstractStochasticMatrix, dims::NTuple{2,IDXT}) where {IDXT} =
+    similar(k, eltype(k), dims)
+similar(k::AbstractStochasticMatrix, ::Type{T}) where {T} = similar(k, T, size(k))
+similar(k::AbstractStochasticMatrix, ::Type{T}, m, n) where {T} = similar(k, T, (m, n))
+
+function similar(
+    k::AbstractStochasticMatrix,
+    ::Type{T},
+    dims::NTuple{2,IDXT},
+) where {T,IDXT}
+    P = probability_matrix(k)
+    Pout = similar(P, T, dims)
+    return StochasticMatrix(Pout, false)
+end
+
 function Base.show(io::IO, K::AbstractStochasticMatrix)
     println(io, summary(K))
     println(io, "P = ")
